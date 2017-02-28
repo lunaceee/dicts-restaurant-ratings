5259105395b0ed8f7d2205000a681d2a40fc9e59
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:56:58 -0800

    added user input to read_ratings function, sorted all the restaurant names as output

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,19 +1,30 @@
 # your code goes here
 
-
-restaurant_ratings = {}
-
 def read_ratings(filename):
-    opened_file = open(filename)
-    for line in opened_file:
-        new_line = line.rstrip()
-        restaurant_and_rating = new_line.split(":")
-        restaurant = restaurant_and_rating[0]
-        restaurant_ratings[restaurant] = restaurant_and_rating[1]
-    restaurants_ordered = sorted(restaurant_ratings.items())
-    for restaurant, rating in restaurants_ordered:
-        print "{} is rated at {}".format(restaurant,rating)
+    """Returns restaurants alphabetically with rating.
 
+    Input: file with restaurant names and ratings.
+    Output: Sorted restaurant names and ratings.
+    """
+    opened_file = open(filename)
+    restaurant_ratings = {}
+
+    user_restaurant = raw_input("What restaurant would you like to add?: "
+                                ).capitalize()
+    user_rating = int(raw_input("What rating do you give the restaurant?: "))
+
+    for line in opened_file:
+        restaurant, rating = line.rstrip().split(":")
+        restaurant_ratings[restaurant] = rating
+
+    restaurant_ratings[user_restaurant] = user_rating
+    restaurants_ordered = sorted(restaurant_ratings.items())
+    
+    for restaurant, rating in restaurants_ordered:
+        print "{} is rated at {}".format(restaurant, rating)
+
+
+    opened_file.close()
 
 
 read_ratings("scores.txt")
